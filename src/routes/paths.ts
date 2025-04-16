5dbabbfb890bd4e2613b3d157a00dccd90665b4c
Munkhjavkhlan <munkhjavkhlann@gmail.com>
2025-04-16 16:18:03 +0800

    feat: feat and paths integrated

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -6,30 +6,17 @@
 
 const ROOTS = {
   AUTH: '/auth',
-  AUTH_DEMO: '/auth-demo',
   DASHBOARD: '/dashboard',
 };
 
-// ----------------------------------------------------------------------
-
 export const paths = {
-  page403: '/error/403',
-  page404: '/error/404',
-  page500: '/error/500',
-  auth: {
-    jwt: {
-      signIn: `${ROOTS.AUTH}/jwt/sign-in`,
-      signUp: `${ROOTS.AUTH}/jwt/sign-up`,
-      otp: `${ROOTS.AUTH}/jwt/otp`,
-    },
-  },
   dashboard: {
-    error: {
-      page403: '/dashboard/error/403',
-      page404: '/dashboard/error/404',
-      page500: '/dashboard/error/500',
-    },
     root: ROOTS.DASHBOARD,
-    blank: `${ROOTS.DASHBOARD}/blank`,
+    posts: {
+      root: `${ROOTS.DASHBOARD}/posts`,
+      new: `${ROOTS.DASHBOARD}/posts/new`,
+    },
+    categories: `${ROOTS.DASHBOARD}/categories`,
+    tags: `${ROOTS.DASHBOARD}/tags`,
   },
 };
